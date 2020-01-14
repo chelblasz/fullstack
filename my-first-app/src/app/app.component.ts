102ed225ad0b69cb984c43a775d98bcc14c2d503
chelblasz <chelseablaszczyk@gmial.com>
2020-01-14 15:09:19 -0700

    hello

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -5,8 +5,6 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-
-//hello
 export class AppComponent {
   title = 'my-first-app';
 
