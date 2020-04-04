bfc75f5f03ca3a72d57f76ed642d33baf16c98c8
chelblasz <chelseablaszczyk@gmial.com>
2020-04-04 14:40:14 -0600

    lastly

--- finalproject/final/src/app/app.component.ts
+++ finalproject/final/src/app/app.component.ts
@@ -7,5 +7,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Build Chore Cards';
-  Person = 'Name()'; //Put input box name 
+
+  public TodoCtrl($scope) {
+  
+    $scope.todos = [
+      {text:'Learn AngularJS', done:false},         
+      {text: 'Build an app', done:false}
+    ];
+    
+    $scope.getTotalTodos = function () {
+      return $scope.todos.length;
+    };
+    
+    
+    $scope.addTodo = function () {
+      $scope.todos.push({text:$scope.formTodoText, done:false});
+      $scope.formTodoText = '';
+    };
+    
+      $scope.clearCompleted = function () {
+          $scope.todos = _.filter($scope.todos, function(todo){
+              return !todo.done;
+          });
+      };
+  }
+  
 }
+
+// public card = 0;
+  // public cards: Array<number> = [];
+
+  // public create() {
+  //   this.cards.push(this.card);
+  // }
--- finalproject/final/src/app/chore.model.ts
+++ finalproject/final/src/app/chore.model.ts
@@ -3,11 +3,17 @@ import {Injectable} from '@angular/core';
 @Injectable()
 
 export class Chores {
-    constructor(
-    public id: string,
-    public location: string,
-    public task: string,
-    public chemical: string,
-    public description: string
-    ) {}
+    public id: string;
+    public location: string;
+    public task: string;
+    
+    constructor(id: string, location: string, task: string) {
+            this.id = id;
+            this.location = location;
+            this.task = task;
+        }
 }
+
+
+ // public chemical: string,
+    // public description: string
